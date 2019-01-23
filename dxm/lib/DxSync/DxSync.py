704afd203fb3fcba3ccf589899ef02685924f038
Marcin Przepiorowski <marcin@delphix.com>
2019-01-23 13:25:23 +0000

    initial

---
+++ dxm/lib/DxSync/DxSync.py
@@ -0,0 +1,91 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2018 by Delphix. All rights reserved.
+#
+# Author  : Marcin Przepiorowski
+# Date    : April 2018
+
+
+import logging
+import pickle
+import json
+from masking_apis.models.export_object_metadata import ExportObjectMetadata
+from masking_apis.apis.sync_api import SyncApi
+from masking_apis.rest import ApiException
+from dxm.lib.DxLogging import print_error
+from dxm.lib.DxLogging import print_message
+
+
+class DxSync(ExportObjectMetadata):
+
+    def __init__(self, engine):
+        """
+        Constructor
+        :param engine: DxMaskingEngine object
+        """
+        ExportObjectMetadata.__init__(self)
+        self.__engine = engine
+        self.__logger = logging.getLogger()
+        self.__synctype = None
+        self.__logger.debug("creating DxSync object")
+
+
+    def from_sync(self, sync):
+        """
+        Copy properties from algorithm object into DxAlgorithm
+        :param column: Algorithm object
+        """
+        self.__dict__.update(sync.__dict__)
+
+    def export(self, name, path=None):
+        """
+        Export algorithm into file
+        :param path: path to save algorithm
+        """
+        api_sync = SyncApi(self.__engine.api_client)
+        self.__logger.debug("Export input %s" % self)
+        export_list = []
+        export_list.append(self)
+        api_response = api_sync.export(export_list)
+        self.__logger.debug("Export response %s" % str(api_response))
+
+        # binary_file = open('{0}.alg'.format(self.algorithm_name), mode='wb')
+        # json.dump(api_response.blob, binary_file)
+        # binary_file.close()
+
+        binary_file = open('{0}.bin'.format(name), mode='wb')
+        pickle.dump(api_response, binary_file)
+        binary_file.close()
+
+
+    def importsync(self, name, path=None):
+        """
+        Import algorithm from file
+        :param path: path to save algorithm
+        """
+
+        binary_file = open('{0}.bin'.format("test53_filers"), mode='rb')
+        syncobj = pickle.load(binary_file)
+        binary_file.close()
+
+
+        api_sync = SyncApi(self.__engine.api_client)
+        self.__logger.debug("Import input %s" % self)
+        api_response = api_sync.import_object(syncobj, force_overwrite=True,
+                        environment_id=2)
+        self.__logger.debug("Import response %s" % str(api_response))
+
+        # binary_file = open('{0}.alg'.format(self.algorithm_name), mode='wb')
+        # json.dump(api_response.blob, binary_file)
+        # binary_file.close()
