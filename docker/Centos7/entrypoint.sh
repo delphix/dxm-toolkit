a018021c002b8643404057653977f3a6c75c46f2
Marcin Przepiorowski <marcin.przepiorowski@delphix.com>
2021-07-29 11:07:37 +0200

    build fix - mac

--- docker/Centos7/entrypoint.sh
+++ docker/Centos7/entrypoint.sh
@@ -4,7 +4,6 @@ cd /github/workspace/dxm/lib/DxEngine
 mv secret.py secret.py.orig
 cat secret.py.orig | sed -e "s/changemechangemechagemechangemec/${INPUT_ENCKEY}/" > secret.py
 
-cat /github/workspace/dxm/lib/DxEngine/secret.py
 
 cd /github/workspace/
 ls -l 
