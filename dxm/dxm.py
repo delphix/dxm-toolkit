3acf1255dcc9741c7839d0fb99cb930e1d618ff2
Marcin Przepiorowski <marcin.przepiorowski@delphix.com>
2021-07-29 11:11:37 +0200

    build fix - mac / version

--- dxm/dxm.py
+++ dxm/dxm.py
@@ -106,7 +106,7 @@ from dxm.lib.DxLogging import logging_est
 from dxm.lib.DxLogging import print_error
 from dxm.lib.DxLogging import print_message
 
-__version__ = "0.8.2.2"
+__version__ = "0.9"
 
 class dxm_state(object):
 
