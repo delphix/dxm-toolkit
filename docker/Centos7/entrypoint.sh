81bcdb1bbefefbc0bacce9cbf6fbfce7fc33baff
Marcin Przepiorowski <marcin.przepiorowski@delphix.com>
2021-07-29 10:27:49 +0200

    build issues

--- docker/Centos7/entrypoint.sh
+++ docker/Centos7/entrypoint.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-# TODO: replace encryption key
+cd /github/workspace/dxm/lib/DxEngine
+mv secret.py secret.py.orig
+cat secret.py.orig | sed -e "s/changemechangemechagemechangemec/${INPUT_ENCKEY}/" > secret.py
 
-# cd /github/workspace/lib
-# mv dbutils.pm dbutils.orig.pm
-# cat dbutils.orig.pm | sed -e "s/put your encryption key here/${INPUT_ENCKEY}/" > dbutils.pm
+cat /github/workspace/dxm/lib/DxEngine/secret.py
 
-
-
-# cd /github/workspace
-# tar czvf /github/workspace/dxtoolkit.tar.gz dxtoolkit2/
-
-# echo ${HOME}
-
-# cp /github/workspace/dxtoolkit.tar.gz ${HOME}
-
-
-cd /github/workspace
+cd /github/workspace/
 ls -l 
 python3 setup.py install
-pip3 install pyinstaller
-# for pyinstaller to run
-pip3 uninstall jeepney -y
-pip3 install jeepney
 pyinstaller --onefile --clean dxmc.py
 cd /github/workspace/dist
 
